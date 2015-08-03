04e53dd5f2227224cd65eab6eed4b7417602a8be
liaolushen <1219810894@qq.com>
2015-08-04 03:24:19 +0800

    correct some bug

--- wechat_main/menuEvent/textResponse.py
+++ wechat_main/menuEvent/textResponse.py
@@ -11,11 +11,11 @@ import xml.etree.ElementTree as ET
 
 def msgRes(data):
     instruction = data.find('Content').text.encode("utf-8")
-    if instruction is "廖医生":
+    if instruction == "廖医生":
         return connectDKF.connectOrderedDKF(data, '1000@health_community')
-    if instruction is "李药师":
+    if instruction == "李药师":
         return connectDKF.connectOrderedDKF(data, '1003@health_community')
-    if instruction is "请求医生帮助":
+    if instruction == "请求医生帮助":
         return connectDKF.connectRandomDKF(data)
     if instruction in ["政策解读", "疫情公布", "服务通知", "政务新闻", "膳食", "周边", "骨松", "糖尿病",
     "体检", "肾病"]:
